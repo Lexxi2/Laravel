07b7128556c105061b875f39aff4fb200dbf2710
Alexa Schneider <alexa@0-1.ch>
2022-07-09 13:45:56 +0200

    first commit, new laravel testproject

---
+++ testproject/resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{ $title }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vitae laboriosam, vel accusantium natus
+        praesentium nulla nesciunt perferendis quos magni odit tempore illo placeat labore nemo expedita totam pariatur
+        obcaecati.</p>
+    
+@endsection
---
+++ testproject/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/services', [PagesController::class, 'services']);
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/about', function(){
+//     return view('pages.about');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is userID '  .$id .' with the name ' .$name;
+});
+
+
+
+
+*/
